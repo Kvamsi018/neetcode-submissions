fd258a00d387505d8e12cc3ce6458b8f431a00de
Kvamsi018 <yvamsi018@gmail.com>
2026-09-01 09:18:27 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int prev[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            prev[i] = -1;
+        }
+
+        prev[0] = 1;
+        prev[1] = 2;
+
+        for(int i=2;i<n;i++){
+            prev[i] = prev[i-2] + prev[i-1];
+        }
+
+        return prev[n-1];
+    }
+}
