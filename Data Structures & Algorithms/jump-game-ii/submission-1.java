3963f782ec8c5f8b737d42075eb76626560b111a
Kvamsi018 <yvamsi018@gmail.com>
2026-09-02 18:40:15 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1000000);
+
+        dp[n-1] = 0;
+        for(int i=n-2;i>=0;i--){
+            int end = Math.min(n,i+nums[i]+1);
+            for(int j=i+1;j<end;j++){
+                dp[i] = Math.min(dp[i], dp[j]+1);
+            }
+        }
+
+        return dp[0];
+    }
+}
