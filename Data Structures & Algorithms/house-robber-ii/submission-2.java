db3c8afb29931cb026b758e7b2f8846c381c42c7
Kvamsi018 <yvamsi018@gmail.com>
2026-09-04 11:50:10 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int robFn(int[] nums, int i, int end, int[] dp){
+        if(i>end) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick = nums[i] + robFn(nums, i+2, end, dp);
+        int nonPick = 0 + robFn(nums, i+1, end, dp);
+        
+        return dp[i] = Math.max(pick, nonPick);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+
+        int case1 = robFn(nums, 0, n-2, dp1);
+        int case2 = robFn(nums, 1, n-1, dp2);
+
+        return Math.max(case1, case2);
+    }
+}
